aba24583ee913c9fd85561499b18acea05d86c1e
Karen Sargsyan <1865030a@gmail.com>
2024-12-07 01:45:28 +0400

    new_one

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dbmyhotel' );
+define( 'DB_NAME', 'myhotelwor_2232976533_database' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'hddzsidsiy' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '8x68XrH2JHZuyrfk' );
+define( 'DB_PASSWORD', 'cexbat-vapga8-niBbeg' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'myhotelwor-2232976533-wpdbserver.mysql.database.azure.com' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8mb3' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
